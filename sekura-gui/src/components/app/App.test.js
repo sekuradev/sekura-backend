65dac864ffdcdbd93319c5cdd772a6cd0ac8995a
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2022-11-12 17:05:20 +0100

    Remove GUI code

--- sekura-gui/src/components/app/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders login by default", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Please sign in/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- sekura-gui/src/services/user.js
+++
@@ -1,5 +0,0 @@
-var session = require("./session");
-
-export function getCurrentUser(userId) {
-  return session.getAxiosSession().get("/api/user/");
-}
